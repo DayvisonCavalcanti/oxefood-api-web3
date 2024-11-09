7b47c958aaa681a9a93d606cf0f946fef6b4d2ad
Dayvison Cavalcanti <dayvisonpaiva8@gmail.com>
2024-11-09 15:01:38 -0300

    configurando o projeto

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiWeb3Application.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiWeb3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiWeb3Application.class, args);
+	}
+
+}
